cbded67069277885937f784e7dfd00151048d9fb
Colton Brenneman <colton.brenneman@gmail.com>
2023-03-01 12:45:19 -0700

    Start of project

---
+++ BYOA_Codable/BYOA_Codable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BYOA_Codable
+//
+//  Created by Colton Brenneman on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
